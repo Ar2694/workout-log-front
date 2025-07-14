ad4f5368226f46dd332802801763cf9e2a9b75a1
Arlix Sorto <arlix2694@gmail.com>
2025-07-13 20:51:52 -0500

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,31 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) =>  {
-    const isDev = mode === "development"
- 
- return{ 
-  plugins: [react(), tsconfigPaths()],
-  css:{
-    modules:{
-      generateScopedName: function (name, filename) {
-        const basename = path.basename(filename, ".css")
-			  const [component]= basename.split(".");
-			return component + "-" + name;
-		},
-    }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: isDev ? "http://localhost:3001": "https://main.workout-log-back-aws.com",
-        changeOrigin: isDev,
-        secure: !isDev
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development"
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    css: {
+      modules: {
+        generateScopedName: function (name, filename) {
+          const basename = path.basename(filename, ".css")
+          const [component] = basename.split(".");
+          return component + "-" + name;
+        },
       }
     },
-    
-  },
-}
-  
+    server: {
+      proxy: {
+        "/api": {
+          target: isDev ? "http://localhost:3001" : "https://main.workout-log-back-aws.com",
+          changeOrigin: !isDev,
+          secure: !isDev,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      },
+
+    },
+  }
+
 })
