3ea14cc0c6f7f77acae07d7730fa1fe15ed03538
Arlix Sorto <arlix2694@gmail.com>
2025-07-19 13:50:11 -0500

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
   const isDev = mode === "development"
-
+  console.log(mode, env.VITE_API_URL, env.VITE_API_URL_DEV,"<<<<++++======/////")
   return {
     plugins: [react(), tsconfigPaths()],
     css: {
@@ -21,9 +22,10 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: isDev ? "http://localhost:3001" : "https://main.workout-log-back-aws.com",
-          changeOrigin: isDev,
-          secure: isDev
+          target: env.VITE_API_URL,
+          changeOrigin: !isDev,
+          secure: !isDev,
+
         }
       },
 
